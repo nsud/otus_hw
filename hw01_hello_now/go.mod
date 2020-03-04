231c6605f8e5c2612b689b3c45f772b019e4c465
User <natali29915021@mail.ru>
2020-03-04 21:51:08 +0300

    HW1 is completed

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -5,6 +5,6 @@ go 1.14
 require (
 	bou.ke/monkey v1.0.2
 	github.com/beevik/ntp v0.2.0
-	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,12 +9,13 @@ import (
 )
 
 func main() {
+	const form = "2006-01-02 15:04:05 +0000 UTC"
 	current := time.Now()
 	exact, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 
-	fmt.Printf("current time: %v \n", current)
+	fmt.Printf("current time: %v\n", current.Format(form))
 
-	fmt.Printf("exact time: %v \n", exact)
+	fmt.Printf("exact time: %v", exact.Format(form))
 	if err != nil {
 		log.Fatal(err)
 	}
