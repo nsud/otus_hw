f0b7f2f5b1e925e2f3548047f7313d62a970c34b
User <natali29915021@mail.ru>
2020-05-11 18:34:17 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,50 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	file, err := os.Open(fromPath)
+	if err != nil {
+		//os.Exit(1)
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+
+	switch {
+	case err != nil:
+		return ErrUnsupportedFile
+	case offset > stat.Size():
+		return ErrOffsetExceedsFileSize
+	case !stat.Mode().IsRegular():
+		return ErrUnsupportedFile
+	case offset != 0:
+		_, err := file.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+		fallthrough
+	case limit == 0:
+		limit = stat.Size() - offset
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(file)
+
+	fileN := toPath + stat.Name()
+	newFile, err := os.Create(fileN)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+	for {
+		_, err := io.CopyN(newFile, barReader, limit)
+		if err == io.EOF {
+			break
+		}
+		return nil
+	}
+	newFile.Chmod(0644)
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,51 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Run("Offset bigger then file size", func(t *testing.T) {
+		offset = 1024
+		from = "./testdata/tesssst.txt"
+		res := Copy(from, to, offset, limit)
+		require.Equal(t, res, errors.New("offset exceeds file size"))
+	})
+	t.Run("Unsupported file", func(t *testing.T) {
+		from = "/dev/urandom"
+		res := Copy(from, to, offset, limit)
+		require.Equal(t, res, errors.New("unsupported file"))
+	})
+	t.Run("Successful copying with limit and offset", func(t *testing.T) {
+		from = "./testdata/t.csv"
+		limit = 50
+		offset = 10
+		Copy(from, to, offset, limit)
+
+		fileSrc, err := os.Open(from)
+		if err != nil {
+			return
+		}
+		defer fileSrc.Close()
+		stSrc, err := fileSrc.Stat()
+		if err != nil {
+			return
+		}
+		sizeOldFile := stSrc.Size()
+
+		fileDst, err := os.Open(to)
+		if err != nil {
+			return
+		}
+		defer fileDst.Close()
+		stDst, err := fileDst.Stat()
+		if err != nil {
+			return
+		}
+		sizeNewFile := stDst.Size()
+		require.Less(t, sizeNewFile, sizeOldFile)
+	})
 }
